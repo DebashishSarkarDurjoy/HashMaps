b22fad2ec677b21e2af4539ca0958893ae29f81b
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2021-12-23 23:06:25 +1100

    ct4 implemented

---
+++ ct4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int count_tri(vector<pair<int, int>> &points) {
+    int ans = 0;
+
+    unordered_map<int, int> freq_x;
+    unordered_map<int, int> freq_y;
+    for (auto p: points) {
+        freq_x[p.first]++;
+        freq_y[p.second]++;
+    }
+
+    for (auto p: points) {
+        int count_x, count_y;
+        count_x = freq_x[p.first];
+        count_y = freq_y[p.second];
+
+        ans += (count_x - 1) * (count_y - 1);
+    }
+
+    return ans;
+}
+
+int main(void) {
+    vector<pair<int, int>> points;
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int x, y;
+        cin >> x >> y ;
+        points.push_back({x, y});
+    }
+
+    cout << "No. of triangles: " << count_tri(points) << endl;
+
+    return 0;
+}
