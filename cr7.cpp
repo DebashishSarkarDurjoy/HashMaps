4fdb55710ccdadfacbe96abbb67fdf4313001d27
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2021-12-25 09:55:42 +1100

    Counting rectangles using pair

--- cr7.cpp
+++ cr7.cpp
@@ -4,37 +4,37 @@
 
 using namespace std;
 
-class Point {
-public:
-    int x;
-    int y;
-
-    Point(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
-};
+// class Point {
+// public:
+//     int x;
+//     int y;
+//
+//     Point(int x, int y) {
+//         this->x = x;
+//         this->y = y;
+//     }
+// };
 
 class Compare {
 public:
-    bool operator()(const Point p1, const Point p2) {
-        if (p1.x == p2.x) return p1.y < p2.y;
-        return p1.x < p2.x;
+    bool operator()(const pair<int, int> p1, const pair<int, int> p2) {
+        if (p1.first == p2.first) return p1.second < p2.second;
+        return p1.first < p2.first;
     }
 };
 
-int count_rect(set<Point, Compare> s) {
+int count_rect(set<pair<int, int>, Compare> s) {
     int ans = 0;
 
     for (auto it = s.begin(); it != prev(s.end()); it++) {
         for (auto jt = next(it); jt != s.end(); jt++) {
-            Point p1 = *it;
-            Point p2 = *jt;
+            pair<int, int> p1 = *it;
+            pair<int, int> p2 = *jt;
 
-            if ( (p1.x == p2.x) || (p1.y == p2.y) ) continue;
+            if ( (p1.first == p2.first) || (p1.second == p2.second) ) continue;
 
-            Point p3(p1.x, p2.y);
-            Point p4(p2.x, p1.y);
+            pair<int, int> p3{p1.first, p2.second};
+            pair<int, int> p4{p2.first, p1.second};
 
             if ( (s.find(p3) != s.end()) && (s.find(p4) != s.end()) ) ans += 1;
         }
@@ -44,7 +44,7 @@ int count_rect(set<Point, Compare> s) {
 }
 
 int main(void) {
-    set<Point, Compare> s;
+    set<pair<int, int>, Compare> s;
     int N;
     cin >> N;
 
@@ -52,7 +52,7 @@ int main(void) {
         int x, y;
         cin >> x >> y;
 
-        Point p(x, y);
+        pair<int, int> p{x, y};
         s.insert(p);
     }
 
