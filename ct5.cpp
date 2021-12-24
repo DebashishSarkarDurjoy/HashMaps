dca615a912fb1b788f8e12ecaffe415e3fefef89
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2021-12-25 09:47:04 +1100

    ct5 practice

---
+++ ct5.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int count_tri(vector<pair<int, int>> &points);
+
+int main(void) {
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> points;
+    for (int i = 0; i < N; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        points.push_back({x, y});
+    }
+
+    cout << "No. of triangles: " << count_tri(points) << endl;
+
+    return 0;
+}
+
+int count_tri(vector<pair<int, int>> &points) {
+    int ans = 0;
+
+    unordered_map<int, int> freq_x;
+    unordered_map<int, int> freq_y;
+    for (auto p: points) {
+        freq_x[p.first]++;
+        freq_y[p.second]++;
+    }
+
+    for (auto p : points) {
+        int count_x = freq_x[p.first];
+        int count_y = freq_y[p.second];
+
+        ans += (count_x - 1) * (count_y - 1);
+        
+    }
+
+    return ans;
+}
